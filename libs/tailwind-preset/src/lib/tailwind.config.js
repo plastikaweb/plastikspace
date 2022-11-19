2a5d298a1e50f00b3430056d4539e4f4f936ba45
Carlos Matheu <info@plastikaweb.com>
2022-11-19 11:42:34 +0100

    feat(nasa-images): #7 add tailwind preset and nasa-images basic config

---
+++ libs/tailwind-preset/src/lib/tailwind.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  theme: {
+    colors: {
+      primary: {
+        light: 'var(--primary-light)',
+        DEFAULT: 'var(--primary)',
+        dark: 'var(--primary-dark)',
+      },
+      secondary: {
+        light: 'var(--secondary-light)',
+        DEFAULT: 'var(--secondary)',
+        dark: 'var(--secondary-dark)',
+      },
+      gray: {
+        5: '#f2f2f2',
+        10: '#e6e6e6',
+        20: '#c9c9c9',
+        30: '#adadad',
+        40: '#919191',
+        50: '#757575',
+        60: '#5c5c5c',
+        70: '#454545',
+        80: '#2e2e2e',
+        90: '#171717',
+      },
+      white: '#ffffff',
+      black: '#000000',
+      transparent: 'transparent',
+    },
+    spacing: {
+      sm: 'var(--spacing-sm)',
+      md: 'var(--spacing-md)',
+      lg: 'var(--spacing-lg)',
+      xl: 'var(--spacing-xl)',
+    },
+    fontSize: {
+      sub: 'var(--font-size-sub)',
+      sm: 'var(--font-size-sm)',
+      base: 'var(--font-size-base)',
+      md: 'var(--font-size-md)',
+      lg: 'var(--font-size-lg)',
+      xl: 'var(--font-size-xl)',
+      xxl: 'var(--font-size-xxl)',
+    },
+  },
+  plugins: [],
+};
