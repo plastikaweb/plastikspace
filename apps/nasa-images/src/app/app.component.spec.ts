15ffe49c31f413d2839d07f7f0d80092d540a573
Carlos Matheu <info@plastikaweb.com>
2022-11-23 16:41:48 +0100

    refactor(nasa-images): #34 remove app.module and make app.component standalone

--- apps/nasa-images/src/app/app.component.spec.ts
+++ apps/nasa-images/src/app/app.component.spec.ts
@@ -1,17 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      imports: [BrowserAnimationsModule, MatSidenavModule, MatIconModule, MatToolbarModule, MatButtonModule],
+      imports: [NoopAnimationsModule, HttpClientModule, AngularSvgIconModule.forRoot()],
     }).compileComponents();
   });
 
--- apps/nasa-images/src/main.ts
+++ apps/nasa-images/src/main.ts
@@ -1,13 +1,22 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { HttpClientModule } from '@angular/common/http';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(RouterModule.forRoot([])),
+    importProvidersFrom(AngularSvgIconModule.forRoot()),
+    importProvidersFrom(HttpClientModule),
+  ],
+}).catch(err => console.error(err));
