680d3ccf2fd9da578b0fbf9929471f2349bcf6d5
Carlos Matheu <info@plastikaweb.com>
2023-03-21 16:48:15 +0100

    feat(nasa-images/search): #82 add a dynamic title for nasa images route
    show the search term and the page number
    
    ISSUES CLOSED: #82

--- apps/nasa-images/src/main.ts
+++ apps/nasa-images/src/main.ts
@@ -2,14 +2,14 @@ import { provideHttpClient } from '@angular/common/http';
 import { enableProdMode, importProvidersFrom, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { provideRouter, TitleStrategy } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { NavigationActionTiming, provideRouterStore } from '@ngrx/router-store';
 import { provideStore, StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { CoreCmsLayoutFeatureModule } from '@plastik/core/cms-layout';
 import { ENVIRONMENT } from '@plastik/core/environments';
-import { CustomRouterSerializer, routerReducers, RouterStateEffects } from '@plastik/core/router-state';
+import { CustomRouterSerializer, PrefixTitleService, routerReducers, RouterStateEffects } from '@plastik/core/router-state';
 import { NasaImagesViews } from '@plastik/nasa-images/entities';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
@@ -42,11 +42,15 @@ bootstrapApplication(AppComponent, {
     ),
     provideRouterStore({
       serializer: CustomRouterSerializer,
-      navigationActionTiming: NavigationActionTiming.PostActivation,
+      navigationActionTiming: NavigationActionTiming.PreActivation,
     }),
     {
       provide: ENVIRONMENT,
       useValue: environment,
+    },
+    {
+      provide: TitleStrategy,
+      useClass: PrefixTitleService,
     },
   ],
   // eslint-disable-next-line no-console
