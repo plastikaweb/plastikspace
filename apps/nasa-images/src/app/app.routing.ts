31831536aaf2e54ebc227d079af1cee3b9c4b571
Carlos Matheu <info@plastikaweb.com>
2023-05-04 15:18:40 +0200

    fix(nasa-images): #192 prevent github pages 404 error on staging
    ISSUES CLOSED: #192

--- apps/nasa-images/src/app/app.routing.ts
+++ apps/nasa-images/src/app/app.routing.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'search' },
   { path: 'search', loadChildren: () => import('@plastik/nasa-images/search').then(routes => routes.nasaImagesSearchFeatureRoutes) },
   { path: 'faqs', loadChildren: () => import('@plastik/nasa-images/faqs').then(routes => routes.nasaImagesFaqsFeatureRoutes) },
-  { path: '**', redirectTo: 'search', pathMatch: 'full' },
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
