939beab54281d1cd3ccd7d56e3d3d654c6f245c9
Carlos Matheu Armengol <info@plastikaweb.com>
2024-08-23 11:17:57 +0200

    feat(llecoop): #273 add categories route head title
    ISSUES CLOSED: #273

--- libs/llecoop/category/feature/src/lib/lib.routes.ts
+++ libs/llecoop/category/feature/src/lib/lib.routes.ts
@@ -1,4 +1,4 @@
 import { Route } from '@angular/router';
 import { LlecoopCategoryFeatureComponent } from './llecoop-category-feature.component';
 
-export const llecoopCategoryFeatureRoutes: Route[] = [{ path: '', component: LlecoopCategoryFeatureComponent }];
+export const llecoopCategoryFeatureRoutes: Route[] = [{ path: '', title: 'Categories de productes', component: LlecoopCategoryFeatureComponent }];
