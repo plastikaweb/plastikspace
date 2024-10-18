b34c9f011f5986797f33502031358c06188a1563
Carlos Matheu Armengol <info@plastikaweb.com>
2024-10-18 12:14:50 +0200

    refactor(llecoop): #414 fix layout to use grid and fixed footer and header
    ISSUES CLOSED: #414

--- libs/llecoop/order-list/feature/llecoop-order-indicator/src/lib/llecoop-order-indicator.component.ts
+++ libs/llecoop/order-list/feature/llecoop-order-indicator/src/lib/llecoop-order-indicator.component.ts
@@ -1,6 +1,7 @@
 import { CurrencyPipe, JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {
   LLecoopOrderListStore,
@@ -10,7 +11,7 @@ import {
 @Component({
   selector: 'plastik-llecoop-order-indicator',
   standalone: true,
-  imports: [MatIconModule, MatBadgeModule, CurrencyPipe, JsonPipe],
+  imports: [MatIconModule, MatBadgeModule, MatButtonModule, CurrencyPipe, JsonPipe],
   templateUrl: './llecoop-order-indicator.component.html',
   styleUrl: './llecoop-order-indicator.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
