0305d249680877c48a33640ac953ceddab7b5cb5
Carlos Matheu Armengol <info@plastikaweb.com>
2024-11-27 12:14:32 +0100

    fix(shared/form/ui): #485 fix specs for InputTableTypeComponent formly component

--- libs/shared/form/ui/table/src/lib/input-table-type.component.spec.ts
+++ libs/shared/form/ui/table/src/lib/input-table-type.component.spec.ts
@@ -1,37 +1,20 @@
-import { signal } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { FormlyModule } from '@ngx-formly/core';
-import { FormlyMaterialModule } from '@ngx-formly/material';
 import { InputTableTypeComponent } from './input-table-type.component';
 
-describe('InputTableComponent', () => {
+describe('InputTableTypeComponent', () => {
   let component: InputTableTypeComponent;
   let fixture: ComponentFixture<InputTableTypeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        InputTableTypeComponent,
-        ReactiveFormsModule,
-        FormlyMaterialModule,
-        FormlyModule.forRoot(),
-        MatInputModule,
-      ],
+      imports: [InputTableTypeComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(InputTableTypeComponent);
     component = fixture.componentInstance;
-    component.props.tableDefinition = {
-      columnProperties: [],
-      count: signal(0),
-    };
-
-    fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
