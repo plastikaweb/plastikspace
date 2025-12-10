4350fe55d21ad581e38f25c2e28534e27cf569f4
Carlos Matheu <info@plastikaweb.com>
2025-12-10 17:44:44 +0100

    fix(eco-store): #768 add preconnect to pb and lcp for 7 images
    ISSUES CLOSED: #768

--- apps/eco-store/src/app/app.component.ts
+++ apps/eco-store/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DOCUMENT, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { POCKETBASE_WITH_TRANSLATION_ENVIRONMENT } from '@plastik/core/environments';
@@ -15,12 +15,21 @@ import { SharedActivityUiOverlayComponent } from '@plastik/shared/activity/ui';
   },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   readonly #translate = inject(TranslateService);
   readonly #environment = inject(POCKETBASE_WITH_TRANSLATION_ENVIRONMENT);
   protected readonly activityStore = inject(activityStore);
+  readonly #document = inject(DOCUMENT);
 
-  constructor() {
+  ngOnInit(): void {
     this.#translate.addLangs(this.#environment.languages);
+    this.addPreconnectLink();
+  }
+
+  private addPreconnectLink(): void {
+    const link = this.#document.createElement('link');
+    link.rel = 'preconnect';
+    link.href = this.#environment.baseApiUrl;
+    this.#document.head.appendChild(link);
   }
 }
